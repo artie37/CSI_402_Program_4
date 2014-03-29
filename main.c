322d95418d364f21df163b418dc50b059cfeb90f
artie37 <artieknapp@Arties-MacBook-Air.local>
2014-03-28 21:34:21 -0400

    03/28/2014: Main file. Contains function calls.

---
+++ main.c
@@ -0,0 +1,19 @@
+// main.c
+// Arthur Knapp
+// AK146316
+
+#include <stdio.h>
+#include "constants.h"
+#include "structs.h"
+#include "globals.h"
+#include "prototypes.h"
+
+int main(int argc, char * argv[])
+{
+    openFiles(argv[1]);
+    // Function call to utilities to open all input and output files
+    loadOpcodes();
+    // Function call to loadOpcodes to insert the opcodes in to the opcode struct array
+    return 0;
+}
+
