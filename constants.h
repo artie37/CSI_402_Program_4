b3b335fed561e865244188906277451b7ead3bc0
artie37 <artieknapp@Arties-MacBook-Air.local>
2014-03-28 21:29:22 -0400

    03/28/2014: Contains the program constant definition.

---
+++ constants.h
@@ -0,0 +1,12 @@
+// constants.h
+// Arthur Knapp
+// AK146316
+
+// This file defines all of the constants used in the program
+
+#define MNEMONIC_SIZE 5
+// Size of the mnemonic character array in the opcodeTable struct
+#define OPTABLE_SIZE 36
+// Size of the opcodeTable struct array
+#define BEGINING_OF_STRING -1
+// Condition to see if i is at the begining of the string
