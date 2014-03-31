0e8f775137b5a875d4d765402e54f324b632c465
artie37 <artieknapp@Arties-MacBook-Air.local>
2014-03-30 20:02:29 -0400

    03/30/2014: Added LINE_SIZE definition.

--- constants.h
+++ constants.h
@@ -10,3 +10,5 @@
 // Size of the opcodeTable struct array
 #define BEGINING_OF_STRING -1
 // Condition to see if i is at the begining of the string
+#define LINE_SIZE 81
+// Max number of characters per line in the source file
