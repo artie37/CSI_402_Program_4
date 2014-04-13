709651fafbf229cdaec9b5e3408123bd26b88631
artie37 <artieknapp@wasp-74-156.potsdam.edu>
2014-04-13 10:40:20 -0400

    04/13/2014: Updated structs header file. Add structure definition for the symbol table.

--- structs.h
+++ structs.h
@@ -6,12 +6,25 @@
 
 // opcodeTable:
 // This struct will be used as an array to store the TMIPS opcode table in memory.
-// It stores the integer value, the mnemonic, and the format of the opcode.
+// It stores the integer opcode value, the mnemonic, the format of the opcode, and
+// the number of operands.
+
+// SymbolTable:
+// This struct will be used as a hash table to store the labels and lc values.
+// It stores the name of the label and the lc value.
 
 typedef struct OpCodeTable
 {
     int opcode;
     char mnemonic[MNEMONIC_SIZE];
     char format;
-    
+    int numOps;
 } OpCodeTable;
+
+typedef struct SymbolTable
+{
+    char name[LABEL_SIZE];
+    int lcValue;
+
+    struct SymbolTable *next;
+} SymbolTable;
