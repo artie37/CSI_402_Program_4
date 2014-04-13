6c1331834f4089f88a47343b0d0b8d996722daec
artie37 <artieknapp@wasp-74-156.potsdam.edu>
2014-04-13 10:39:06 -0400

    04/13/2014: Updated macros header file. Macro definitions are organized by the files and functions they are used in.

--- macros.h
+++ macros.h
@@ -2,7 +2,35 @@
 // Arthur Knapp
 // AK146316
 
-//------------------------------------------------------------------------------------------------------------------------------------
+//************************************************************************************************************************************
+
+// File: main.c
+
+// Opening Files:
+
+#define openOpcodeFile(opcodeFile) ((opcodeFile = fopen("opcodes", "r")) == NULL)
+// Opens the opcode file and checks if it doesn't open
+#define openAsmFile(asmFile, asmName) ((asmFile = fopen(asmName, "r")) == NULL)
+// Opens the assembly file and checks if it doesn't open
+#define openSymFile(symFile, symName) ((symFile = fopen(symName, "w")) == NULL)
+// Opens the symbol table file and checks if it doesn't open
+#define openObjFile(objFile, objName) ((objFile = fopen(objName, "w")) == NULL)
+// Opens the object file and checks if it doesn't open
+#define openErrFile(errFile, errName) ((errFile = fopen(errName, "w")) == NULL)
+// Opens the error file and checks if it doesn't open
+
+// Closing Files:
+
+#define closeAsmFile(asmFile) (fclose(asmFile) != 0)
+// Closes the assembly file and checks if it doesn't close
+#define closeSymFile(symFile) (fclose(symFile) != 0)
+// Closes the symbol table file and checks if it doesn't close
+#define closeObjFile(objFile) (fclose(objFile) != 0)
+// Closes the object file and checks if it doesn't close
+#define closeErrFile(errFile) (fclose(errFile) != 0)
+// Closes the error file and checks if it doesn't close
+
+//************************************************************************************************************************************
 
 // File: utilities.c
 
@@ -20,29 +48,57 @@
 #define noExt(i, j) ((i) == (j))
 // Checks if character array index is at the beginning of the array
 
-// Opening Files:
-
-#define openOpcodeFile(opcodeFile) ((opcodeFile = fopen("opcodes", "r")) == NULL)
-// Opens the opcode file and checks if it doesn't open
-#define openAsmFile(asmFile, asmName) ((asmFile = fopen(asmName, "r")) == NULL)
-// Opens the assembly file and checks if it doesn't open
-#define openSymFile(symFile, symName) ((symFile = fopen(symName, "w")) == NULL)
-// Opens the symbol table file and checks if it doesn't open
-#define openObjFile(objFile, objName) ((objFile = fopen(objName, "w")) == NULL)
-// Opens the object file and checks if it doesn't open
-#define openErrFile(errFile, errName) ((errFile = fopen(errName, "w")) == NULL)
-// Opens the error file and checks if it doesn't open
-
 // Closing Files:
 
 #define closeOpcodeFile(opcodeFile) (fclose(opcodeFile) != 0)
 // Closes the opcode file and checks if it doesn't close
 
-//------------------------------------------------------------------------------------------------------------------------------------
+//************************************************************************************************************************************
 
 // File: functions.c
 
-// Function: errorChecker********************************************************************************
+// Function: firstPass~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// While Loop Conditions
+
+#define tokNotNull(label) (label != NULL)
+// Checks if the current token is not null to keep looping
+
+// If Statement Conditions
+
+#define isValidChar(line, i) ((line[i] != ' ') && (line[i] != '\t') && (line[i] != '\n') && (line[i] != '#'))
+// Checks if the current character is not a whitespace, tab, newline character, or a '#' signifying the current
+// character is not whitespace or a comment symbol.
+#define isLabel(token, tokLength) (token[tokLength] == ':')
+// Checks if the current token is a label by checking to see if the last character of the token is a ':'
+#define isPseudoOp(token) (token[0] == '.')
+// Checks if the current token is a pseudo opcode by checking to see if the first character of token is a '.'
+#define isResw(token) (strcmp(token, ".resw") == 0)
+// Checks if the current token is ".resw"
+#define isWord(token) (strcmp(token, ".word") == 0)
+// Checks if the current token is ".word"
+#define isDataOrText(token) (strcmp(token, ".data") == 0 || strcmp(token, ".text") == 0)
+// Checks if the current token is ".data" or ".text"
+
+// Function: secondPass~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// While Loop Conditions:
+
+#define opTable(x) (x <= 35)
+// Loops through the opcode table for each entry
+
+// If Statement Conditions:
+
+#define isOpcode(mnemonic, token) (strcmp(mnemonic, token) == 0)
+// Checks if the current token is a valid opcode
+#define isFirstOperand(numOps) (numOps > 0)
+// Checks if there are more than 0 operands
+#define isSecondOperand(numOps) (numOps > 1)
+// Checks if there are more than 1 operands
+#define isThirdOperand(numOps) (numOps > 2)
+// Checks if there are more than 2 operands
+
+// Function: errorChecker~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 // While Loop Conditions:
 
@@ -58,3 +114,14 @@
 // character is not whitespace.
 #define isWhitespace(charFlag) (charFlag == 0)
 // Checks if the charFlag remained 0 signifying that the line is whitespace
+
+//************************************************************************************************************************************
+
+// File: hashFunctions.c
+
+// Function: insert~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#define isUpperCase(token) ((token[0] >= 65) && (token[0] <= 90))
+// Checks if the ascii value of the first character is between 65 and 90 inclusive signifying a upper case character
+#define isLowerCase(token) ((token[0] >= 97) && (token[0] <= 122))
+// Checks if the ascii value of the first character is between 97 and 122 inclusive signifying a lower case character
