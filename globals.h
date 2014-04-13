f37d3924d747e97abd90b7600126c236c1cde814
artie37 <artieknapp@wasp-74-156.potsdam.edu>
2014-04-13 10:36:33 -0400

    04/13/2014: Updated globals header file

--- globals.h
+++ globals.h
@@ -4,11 +4,3 @@
 
 OpCodeTable opCodeTable[OPTABLE_SIZE];
 // Used to store the opCodeTable struct array
-FILE *asmFile;
-// Used to reference the input assembly file
-FILE *objFile;
-// Used to reference the output object file
-FILE *symFile;
-// Used to reference the output symbol table file
-FILE *errFile;
-// Used to reference the output error file
