ce1f05635abdd363d76fa50d00b49b65d53755fb
artie37 <artieknapp@wasp-74-156.potsdam.edu>
2014-04-13 10:35:40 -0400

    04/13/2014: Updated constants header file

--- constants.h
+++ constants.h
@@ -8,7 +8,15 @@
 // Size of the mnemonic character array in the opcodeTable struct
 #define OPTABLE_SIZE 36
 // Size of the opcodeTable struct array
-#define BEGINING_OF_STRING -1
+#define BEGINNING_OF_STRING -1
 // Condition to see if i is at the begining of the string
 #define LINE_SIZE 81
 // Max number of characters per line in the source file
+#define LABEL_SIZE 50
+// Max number of characters in a label
+#define UPPER_CASE_ASCII 65
+// The ascii value of upper case a
+#define LOWER_CASE_ASCII 97
+// The ascii value of lower case a
+#define HASH_SIZE 26
+// Size of the hash table
---
+++ hashFunctions.c
@@ -0,0 +1,9 @@
+//
+//  hashFunctions.c
+//  Program_4
+//
+//  Created by Arthur Knapp on 4/5/14.
+//  Copyright (c) 2014 artie37. All rights reserved.
+//
+
+#include <stdio.h>
