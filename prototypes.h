20736aedc99d8ef5bf6f6bc0a86c226e55b25b5a
artie37 <artieknapp@Arties-MacBook-Air.local>
2014-04-18 09:54:44 -0400

    04/18/2014: Final Version

--- prototypes.h
+++ prototypes.h
@@ -11,11 +11,39 @@ char * errorName(char *);
 
 // File: functions.c--------------------------------------------------------------------------------------------------------------------------
 
-void firstPass(FILE *);
-void secondPass(FILE *);
-void errorChecker(FILE *);
+ErrorCodes firstPass(FILE *);
+ErrorCodes secondPass(FILE *);
+void errorHandler(FILE *, char *, int, int, int);
 
 // File: hashFunctions.c----------------------------------------------------------------------------------------------------------------------
 
+void initializeTableToNull();
 int hashValue(char *);
-void insert(char *);
+int insert(char *, int);
+void createList(char *, int, int);
+void writeTable(char *);
+int findUndefinedLabel(char *);
+int findLabelAddress(char *);
+
+// File: errorLinkedLists.c-------------------------------------------------------------------------------------------------------------------
+
+void insertUndefinedLabel(char *);
+void insertMultDefinedLabel(char *);
+void insertIllegalOpcode(char *, int);
+void createUndefinedLabelList(char *);
+void createMultDefinedLabelList(char *);
+void createIllegalOpcodeList(char *, int);
+void printIllegalOpcodeList(FILE *);
+void printUndefinedLabelList(FILE *);
+void printMultDefinedLabelList(FILE *);
+
+// File: objectFunctions.c
+
+void assembleR(int, int, int, int, int, int);
+void assembleJ(int, int, int, int, char *);
+void assembleI();
+int assembleResw(int, int);
+int assembleWord(int, int, int);
+void insertAssembledValues(int, int);
+void createAssembledValuesList(int, int);
+void printAssembledValuesList(FILE *);
