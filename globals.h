1e506d08ac3120cefb47bb599ce2aae5d6b043c2
artie37 <artieknapp@Arties-MacBook-Air.local>
2014-04-18 09:55:02 -0400

    04/18/2014: Final Version

--- globals.h
+++ globals.h
@@ -4,3 +4,5 @@
 
 OpCodeTable opCodeTable[OPTABLE_SIZE];
 // Used to store the opCodeTable struct array
+ErrorCodes errorCodes;
+// Stores the error codes to be returned to the main function from second pass
