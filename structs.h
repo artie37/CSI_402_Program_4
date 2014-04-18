9a333547fd9de1cf45c9741f4bfd2282d5f76f45
artie37 <artieknapp@Arties-MacBook-Air.local>
2014-04-18 09:55:34 -0400

    04/18/2014: Final Version

--- structs.h
+++ structs.h
@@ -13,6 +13,24 @@
 // This struct will be used as a hash table to store the labels and lc values.
 // It stores the name of the label and the lc value.
 
+// LinkedListUndefinedLabel:
+// This struct will be used as a linked list for undefined labels. It stores the label
+
+// LinkedListMultDefinedLabel:
+// This struct will be used as a linked list for multiply defined labels. It stores the label.
+
+// LinkedListIllegalMachineOpcode:
+// This struct will be used as a linked list for illegal opcode. It stores the opcode and the
+// line number that the illegal opcode occured on.
+
+// ErrorCodes:
+// This struct will be used to store the error code flags and return them. These are stored in
+// a struct because returning multiple parameters is not permmited.
+
+// AssembledValues:
+// This struct will be used as a linked list for assembled values. It stores the lc value of the
+// instruction and the contents of the instruction.
+
 typedef struct OpCodeTable
 {
     int opcode;
@@ -28,3 +46,47 @@ typedef struct SymbolTable
 
     struct SymbolTable *next;
 } SymbolTable;
+
+typedef struct LinkedListUndefinedLabel
+{
+    char name[LABEL_SIZE];
+    
+    struct LinkedListUndefinedLabel *next;
+}UndefinedLabelList;
+
+typedef struct LinkedListMultDefinedLabel
+{
+    char name[LABEL_SIZE];
+    
+    struct LinkedListMultDefinedLabel *next;
+}MultDefinedLabelList;
+
+typedef struct LinkedListIllegalMachineOpcode
+{
+    char mnemonic[MNEMONIC_SIZE];
+    int lineNum;
+    
+    struct LinkedListIllegalMachineOpcode *next;
+}IllegalOpcodeList;
+
+typedef struct ErrorCodes
+{
+    int invalidOpcodeFlag;
+    // Flag signifying whether or not the opcode is valid or not. 1 = invalid, 0 = valid.
+    int undefinedLabelFlag;
+    // Flag signifying whether or not the label is defined. 1 = undefined, 0 = defined.
+    int multDefinedLabelFlag;
+    // Flag signifying whether or not the label is multiply defined.
+    // 1 = multiply defined, 0 = singly defined.
+}ErrorCodes;
+
+typedef struct AssembledValues
+{
+    int lcValue;
+    // LC Value of instruction
+    int contents;
+    // Contents of instruction
+    
+    struct AssembledValues *next;
+}AssembledValues;
+
