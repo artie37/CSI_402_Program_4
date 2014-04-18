3ef98801fb229c2696b4b889bb51e688b48f09cf
artie37 <artieknapp@Arties-MacBook-Air.local>
2014-04-18 09:58:18 -0400

    04/18/2014: Final Version

--- main.c
+++ main.c
@@ -22,10 +22,6 @@ int main(int argc, char * argv[])
     // Used to reference the input assembly file
     FILE *objFile;
     // Used to reference the output object file
-    FILE *symFile;
-    // Used to reference the output symbol table file
-    FILE *errFile;
-    // Used to reference the output error file
     char *symName;
     // Stores the name of the symbol file
     char *objName;
@@ -40,42 +36,52 @@ int main(int argc, char * argv[])
         exit(1);
     }
     
+    if invalidAmountOfCommands(argc)
+    // Checks if there are an incorrect amount of commands
+    {
+        fprintf(stderr, "Error: Incorrect Number Of Commands\n");
+        exit(1);
+    }
+    
     symName = symbolName(argv[1]);
     objName = objectName(argv[1]);
     errName = errorName(argv[1]);
-    
-    printf("Symbol Table File Name: %s\n", symName);
-    printf("Object File Name: %s\n", objName);
-    printf("Error File Name: %s\n", errName);
-    
-    if openSymFile(symFile, symName)
-    // Open symbol table file. If it doesn't open, print error message and close program.
-    {
-        fprintf(stderr, "Error: Symbol Table File Did Not Open\n");
-        exit(1);
-    }
-    
-    if openObjFile(objFile, objName)
-    // Open object file. If it doesn't open, print error message and close program.
-    {
-        fprintf(stderr, "Error: Object File Did Not Open\n");
-        exit(1);
-    }
-    
-    if openErrFile(errFile, errName)
-    // Open error file. If it doesn't open, print error message and close program.
-    {
-        fprintf(stderr, "Error: Error File Did Not Open\n");
-        exit(1);
-    }
+    // Function calls to get correct file names
 
+    initializeTableToNull();
+    // Function call to initializeTableToNull
+    errorCodes = firstPass(asmFile);
+    // Function call to firstPass
+    rewind(asmFile);
     loadOpcodes();
     // Function call to loadOpcodes
-    firstPass(asmFile);
-    // Function call to firstPass
-    errorChecker(asmFile);
-    // Function call to errorChecker
+    errorCodes = secondPass(asmFile);
     
+    if isAnError(errorCodes.invalidOpcodeFlag, errorCodes.undefinedLabelFlag, errorCodes.multDefinedLabelFlag)
+    // Checks if there is an illegal opcode, undefined label, multiply defined label and makes error file
+    {
+        rewind(asmFile);
+        errorHandler(asmFile, errName, errorCodes.invalidOpcodeFlag,
+                     errorCodes.undefinedLabelFlag, errorCodes.multDefinedLabelFlag);
+        // Function call to errorChecker
+    }
+    
+    else
+    {
+        writeTable(symName);
+        // Function call to writeTable to write the symbol table to the .sym file
+        
+        if openObjFile(objFile, objName)
+            // Open object file. If it doesn't open, print error message and close program.
+        {
+            fprintf(stderr, "Error: Object File Did Not Open\n");
+            exit(1);
+        }
+        
+        printAssembledValuesList(objFile);
+        // Function call to printAssembledValuesList to write the lc values and contents to .obj file
+    }
+
     return 0;
 }
 
