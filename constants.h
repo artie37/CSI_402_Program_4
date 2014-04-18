e0a75a9c3a4fa97e31a0b3b4023f64739746bc3d
artie37 <artieknapp@Arties-MacBook-Air.local>
2014-04-18 09:57:13 -0400

    04/18/2014: Final Version

--- constants.h
+++ constants.h
@@ -4,7 +4,7 @@
 
 // This file defines all of the constants used in the program
 
-#define MNEMONIC_SIZE 5
+#define MNEMONIC_SIZE 81
 // Size of the mnemonic character array in the opcodeTable struct
 #define OPTABLE_SIZE 36
 // Size of the opcodeTable struct array
@@ -12,7 +12,7 @@
 // Condition to see if i is at the begining of the string
 #define LINE_SIZE 81
 // Max number of characters per line in the source file
-#define LABEL_SIZE 50
+#define LABEL_SIZE 81
 // Max number of characters in a label
 #define UPPER_CASE_ASCII 65
 // The ascii value of upper case a
@@ -20,3 +20,33 @@
 // The ascii value of lower case a
 #define HASH_SIZE 26
 // Size of the hash table
+#define FIRST_SHIFT 26
+// Amount to shift
+#define SECOND_SHIFT 21
+// Amount to shift
+#define THIRD_SHIFT 16
+// Amount to shift
+#define FOURTH_SHIFT 11
+// Amount to shift
+#define FIFTH_SHIFT 6
+// Amount to shift
+#define POSITIVE_NUMBER -32769
+// Mask of positive number
+#define NEGATIVE_NUMBER 65535
+// Mask of negative number
+#define SHIFT_FIFTEEN 15
+// Amount to shift
+#define SHIFT_THIRTY_ONE 31
+// Amount to shift
+#define SET_MASK 255
+// Amount to set the mask
+#define SHIFT_EIGHT 8
+// Amount to shift
+#define SHIFT_TWENTYFOUR 24
+// Amount to shift
+#define UPPER_CASE 65
+// Ascii value of upper case a
+#define LOWER_CASE 97
+// Ascii value of lower case a
+#define EXTENSION 4
+// Amount to add file name to store file extension
